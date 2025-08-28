229176b60bce486bc6545483257e2e459ebfa9d2
Shamsu-vai <shamspahlowan03@gmail.com>
2025-08-29 04:21:43 +0600

    refined home screen structure

--- lib/features/product_browsing/presentation/home_screen.dart
+++ lib/features/product_browsing/presentation/home_screen.dart
@@ -1,75 +1,211 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
+    final double expandedHeight = MediaQuery.of(context).size.height * 0.2;
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Color(0xFF37917A),
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.shifting,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history_outlined),
+            label: "History",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+        ],
+        onTap: (value) {
+          setState(() {});
+        },
+      ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
             stretch: true,
             pinned: true,
-            expandedHeight: 178,
+            floating: true,
+            expandedHeight: expandedHeight,
             backgroundColor: const Color(0xFF20CFA3),
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
-              title: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: SizedBox(
-                  height: 35,
-                  child: SearchBar(
-                    padding: WidgetStatePropertyAll(
-                      EdgeInsetsGeometry.symmetric(horizontal: 12),
+              titlePadding: EdgeInsets.all(12),
+              expandedTitleScale: 1.2,
+              title: SizedBox(
+                height: 40,
+                child: SearchBar(
+                  padding: WidgetStatePropertyAll(
+                    EdgeInsetsGeometry.symmetric(horizontal: 12),
+                  ),
+                  hintText: 'Search products',
+                  leading: const Icon(Icons.search),
+                  onChanged: (query) {},
+                  elevation: WidgetStateProperty.all(0),
+                  backgroundColor: WidgetStateProperty.all(
+                    const Color(0xFFF0F0F0),
+                  ),
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    elevation: WidgetStatePropertyAll(0),
-                    hintText: "search products",
-                    trailing: [Icon(Icons.search_rounded)],
                   ),
                 ),
               ),
               background: Align(
                 alignment: Alignment.center,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    expandedLeading(),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(Icons.location_pin),
-                        Text("Dhaka, Bangladesh"),
-                      ],
-                    ),
-                    const Text("Jahangir Gate, Tejgaon."),
-                    const SizedBox(height: 30),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      expandedLeading(),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: const [
+                          Icon(Icons.location_pin, size: 30),
+                          Text(
+                            "Dhaka, Bangladesh",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        "Jahangir Gate, Tejgaon.",
+                        style: TextStyle(
+                          // fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
           SliverToBoxAdapter(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              child: ListView.builder(
-                itemCount: 100,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    height: 100,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                    child: Center(child: Text(index.toString())),
-                  );
-                },
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Container(
+                padding: EdgeInsets.all(12),
+                height: 180,
+                child: PageView.builder(
+                  controller: PageController(viewportFraction: 0.9),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(child: Text("vegetable")),
+                    );
+                  },
+                ),
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Catagories",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(12),
+                  height: 120,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(child: Text("groceries")),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Popular Products",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(12),
+                  height: 300,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        padding: EdgeInsets.all(12),
+
+                        // height: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: Center(child: Text("products")),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -81,7 +217,7 @@ class HomeScreen extends StatelessWidget {
     return Row(
       children: [
         CircleAvatar(
-          radius: 28,
+          radius: 24,
           backgroundColor: Colors.white,
           child: Padding(
             padding: const EdgeInsets.all(6),
@@ -95,16 +231,16 @@ class HomeScreen extends StatelessWidget {
           children: const [
             Text(
               "Shams Pahlowan",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
-            Text("usefull information", style: TextStyle(fontSize: 14)),
+            Text("useful information", style: TextStyle(fontSize: 12)),
           ],
         ),
         const Expanded(child: SizedBox()),
         IconButton(
           iconSize: 32,
           onPressed: () {},
-          icon: const Icon(Icons.settings),
+          icon: const Icon(Icons.settings, color: Colors.black),
         ),
       ],
     );
